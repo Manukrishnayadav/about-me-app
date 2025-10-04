2fecb3fefbbdbf43725b1611cdf6399442f448cb
Manukrishnayadav <manukrishnayadav1@gmail.com>
2025-10-04 12:06:23 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Server is running');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
